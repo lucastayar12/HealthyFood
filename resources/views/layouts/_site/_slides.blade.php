b46fb53c8799ff579e2d757c98b84812182f18d0
lucastayar12 <lucastayar12@gmail.com>
2019-11-08 11:47:00 -0300

    REPO EDIT SLIDE

--- resources/views/layouts/_site/_slides.blade.php
+++ resources/views/layouts/_site/_slides.blade.php
@@ -3,8 +3,8 @@
 		<li>
 			<img src="{{asset('imagens/kek1.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h3>Inclua frutas nas refeições</h3>
-				<h5>Ricas em nutrientes e também deliciosas, as saladas que combinam frutas e verduras podem ser uma boa saída para aumentar o consumo diário desse alimento. Em alguns casos, a união pode até se complementar, como acontece com a couve verde e a laranja.</h5>
+				<h3>Título da Imagem</h3>
+				<h5>Descrição do Slide</h5>
 			</div>	
 		</li>
 		<li>
