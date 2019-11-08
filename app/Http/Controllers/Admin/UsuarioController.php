4f9b963f48e91007586a0324208beabc05ea6402
lucastayar12 <lucastayar12@GMAIL.COM>
2019-11-08 16:42:44 -0300

    Update UsuarioController.php

--- app/Http/Controllers/Admin/UsuarioController.php
+++ app/Http/Controllers/Admin/UsuarioController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests;
 
 class UsuarioController extends Controller
 {
     public function login(Request $request)
     {
     	$dados = $request->all();
-    	dd()
+    	dd($dados);	
+
     }
 }
