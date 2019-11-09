ecdb854731a1f3049b6ededf980853064bc0ebbe
lucastayar12 <lucastayar12@gmail.com>
2019-11-09 16:05:56 -0300

    JS Filtro

---
+++ resources/views/layouts/_site/_filtros.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+	<form>
+		<div class="input-field col s6 m4">
+			<select>
+				<option value="1">Verdura</option>
+				<option value="2">Legume</option>
+				<option value="3">Fruta</option>
+			</select>
+			<label>Tipo</label>
+		</div>
+	</form>
+</div>
