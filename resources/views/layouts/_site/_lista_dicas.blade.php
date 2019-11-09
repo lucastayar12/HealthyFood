97f533eb86c9f87b58424b42bc8f620d731a6d7f
KRSilva <lucastayar12@GMAIL.COM>
2019-11-09 18:19:28 -0300

    pagina principal implementacao

--- resources/views/layouts/_site/_lista_dicas.blade.php
+++ resources/views/layouts/_site/_lista_dicas.blade.php
@@ -7,11 +7,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/azeite.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #001</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1"></b></p>
+				<p>O azeita assusta quando é encontrado no topo de qualquer lista de alimentos saudáveis para emagrecer. Afinal de contas, não é rico em gorduras? Sim, mas existem gorduras que podem ajudar a emagrecer. É o caso das gorduras poli-insaturadas, que reduzem a dor provocada pela fome. O azeite possui ainda o ácido oleico, responsável pela quebra de gorduras em excesso no corpo</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -22,11 +22,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/folhasverde.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #002</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Folhas Verdes</b></p>
+				<p>Folhas verdes como a couve e o alface possuem uma carga de calorias extremamente baixas, mas fornecem uma grande quantidade de fibras, que provocam sensação de saciedade. Além disso, aliviam a retenção de água pelo corpo.</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -37,11 +37,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/caldo.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #003</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Sopas</b></p>
+				<p>Sopas, desde que não sejam muito calóricas, são alimentos saudáveis para emagrecer. Ao comer uma pequena porção de sopa antes de cada refeição, muitas pessoas acabam comendo menos do que comeriam normalmente.</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -52,11 +52,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/graos.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #004</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Grãos e sementes</b></p>
+				<p>Por serem uma forma prática de adicionar mais saúde aos pratos, e inserida nos mais diversos alimentos, as sementes e os grãos podem ser consumidas diariamente, pois são ricos em nutrientes essenciais para o bom funcionamento do nosso corpo.</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -97,11 +97,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/caldo.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/frutas.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #007</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Frutas</b></p>
+				<p>A fruta é peça fundamental numa alimentação saudável e devem ser consumidas diariamente. Elas fornecem vitaminas, minerais, diferentes fibras alimentares, compostos protetores que ajudam a regular o organismo e  antioxidantes que são nutrientes essenciais na proteção das células. Em conjunto, estes nutrientes têm propriedades protetoras que fazem das frutas um alimento vital.</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -112,11 +112,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/caldo.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/paes.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #008</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Pães integrais</b></p>
+				<p>A massa dele é muito mais rica em fibras, vitaminas e minerais. Ao optar pelo pão integral, o corpo ganha benefícios incontáveis, porque este tipo de pão tem menos açúcares e gorduras, o que ajuda na função intestinal e aumenta a sensação de saciedade após a refeição</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
