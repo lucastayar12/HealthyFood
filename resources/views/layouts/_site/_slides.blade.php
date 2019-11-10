c0e914d70bd230cecf01fde438a3513030b11681
lucastayar12 <lucastayar12@gmail.com>
2019-11-10 15:23:01 -0300

    css

--- resources/views/layouts/_site/_slides.blade.php
+++ resources/views/layouts/_site/_slides.blade.php
@@ -1,29 +1,28 @@
 <div class="slider">
 	<ul class="slides" id="slides-1">
-		<li>
+		<li>	
 			<img src="{{asset('imagens/slide1.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h5><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="10px" height="10px"></a></h5>
-				
+				<h3>HealthyFood</h3>
 			</div>	
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide2.jpg')}}" alt="Imagem">
-					<h1><img class="circle responsive-img" src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50px" height="50px"></h1>
+			<div class="caption left-align">
+				<h3>HealthyFood</h3>
+			</div>
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide3.jpg')}}" alt="Imagem">
 			<div class="caption right-align">
-				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
-				
+				<h3>HealthyFood</h3>
 			</div>	
 		</li>
 
 		<li>
 			<img src="{{asset('imagens/slide4.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
-				
+				<h3>HealthyFood</h3>
 			</div>	
 		</li>
 		
