31f12510c551695dd4bf1e3f024d10ce7840f938
KRSilva <lucastayar12@GMAIL.COM>
2019-11-10 01:52:08 -0300

    imgs

--- resources/views/layouts/_site/_slides.blade.php
+++ resources/views/layouts/_site/_slides.blade.php
@@ -3,21 +3,18 @@
 		<li>
 			<img src="{{asset('imagens/slide1.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h3>Sucos</h3>
+				<h5><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="10px" height="10px"></a></h5>
 				
 			</div>	
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide2.jpg')}}" alt="Imagem">
-			<div class="caption left-align">
-				<h3>Titulo da Imagem</h3>
-				
-			</div>	
+					<h1><img class="circle responsive-img" src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50px" height="50px"></h1>
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide3.jpg')}}" alt="Imagem">
 			<div class="caption right-align">
-				<h3>Titulo da Imagem</h3>
+				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
 				
 			</div>	
 		</li>
@@ -25,7 +22,7 @@
 		<li>
 			<img src="{{asset('imagens/slide4.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h3>Titulo da Imagem</h3>
+				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
 				
 			</div>	
 		</li>
