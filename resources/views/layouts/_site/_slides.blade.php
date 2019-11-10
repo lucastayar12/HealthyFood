757c30c75c0b7d095e26316ab8d19864f52b0746
KRSilva <lucastayar12@GMAIL.COM>
2019-11-10 10:25:39 -0300

    footer,listasdicas,slides

--- resources/views/layouts/_site/_slides.blade.php
+++ resources/views/layouts/_site/_slides.blade.php
@@ -3,18 +3,19 @@
 		<li>
 			<img src="{{asset('imagens/slide1.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h5><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="10px" height="10px"></a></h5>
+				<h3>Sucos</h3>
 				
 			</div>	
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide2.jpg')}}" alt="Imagem">
-					<h1><img class="circle responsive-img" src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50px" height="50px"></h1>
+			<div class="caption right-align">
+					<h3>Proteinas</h3>
 		</li>
 		<li>
 			<img src="{{asset('imagens/slide3.jpg')}}" alt="Imagem">
 			<div class="caption right-align">
-				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
+				<h3>Folhas</h3>
 				
 			</div>	
 		</li>
@@ -22,7 +23,7 @@
 		<li>
 			<img src="{{asset('imagens/slide4.jpg')}}" alt="Imagem">
 			<div class="caption center-align">
-				<h3><a href="#"><img src="{{asset('imagens/logo final.png')}}" alt="Imagem" width="50" height="50"></a></h3>
+				<h3>Verduras</h3>
 				
 			</div>	
 		</li>
