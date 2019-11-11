ae7e20641359fa087b7df1f44038eb46e1b84914
lucastayar12 <lucastayar12@GMAIL.COM>
2019-11-11 07:18:08 -0300

    eita

--- resources/views/layouts/_site/_lista_dicas.blade.php
+++ resources/views/layouts/_site/_lista_dicas.blade.php
@@ -7,11 +7,13 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/azeite.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #001</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Azeite</b></p>
+				<p>O azeita assusta quando é encontrado no topo de qualquer lista de alimentos saudáveis para emagrecer. Afinal de contas, não é rico em gorduras? Sim, mas existem gorduras que podem ajudar a emagrecer. É o caso das gorduras poli-insaturadas, que reduzem a dor provocada pela fome.
+O azeite possui ainda o ácido oleico, responsável pela quebra de gorduras em excesso no corpo
+</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
@@ -22,11 +24,11 @@
 	<div class="col s12 m3">
 		<div class="card">
 			<div class="card-image">
-				<a href="#"><img src="{{asset('imagens/alimento1.jpg')}}" alt="Imagem"></a>
+				<a href="#"><img src="{{asset('imagens/folhasverdes.jpg')}}" alt="Imagem"></a>
 			</div>
 			<div class="card-content">
-				<p><b class="deep-orange-text darken-1">Dica #002</b></p>
-				<p>BLA BLA BLA DICA AQUI BLA BLA BLA...</p>
+				<p><b class="deep-orange-text darken-1">Folhasverdes</b></p>
+				<p>Folhas verdes como a couve e o alface possuem uma carga de calorias extremamente baixas, mas fornecem uma grande quantidade de fibras, que provocam sensação de saciedade. Além disso, aliviam a retenção de água pelo corpo.</p>
 			</div>
 			<div class="card-action">
 				<a href="#">Ver Mais</a>
