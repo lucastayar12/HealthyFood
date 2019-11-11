000f2e718261d74a5d551946608b7b209ae8953e
lucastayar12 <lucastayar12@gmail.com>
2019-11-10 23:20:32 -0300

    asd

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,19 @@ Route::middleware(['auth', 'auth.basic'])->group(function () {
 	Route::put('admin/paginas/atualizar/{id}',['as' => 'admin.paginas.atualizar', 'uses' => 'Admin\PaginaController@atualizar']);
 
 
+	Route::get('/admin/tipos', ['as' => 'admin.tipos', 'uses' => 'Admin\TipoController@index']);
+
+	Route::get('/admin/tipos/adicionar', ['as' => 'admin.tipos.adicionar', 'uses' => 'Admin\TipoController@adicionar']);
+
+	Route::get('/admin/tipos/salvar', ['as' => 'admin.tipos.salvar', 'uses' => 'Admin\TipoController@salvar']);
+
+	Route::get('/admin/tipos/editar/{id}', ['as' => 'admin.tipos.editar', 'uses' => 'Admin\TipoController@editar']);
+
+	Route::put('/admin/tipos/atualizar/{id}', ['as' => 'admin.tipos.atualizar', 'uses' => 'Admin\TipoController@atualizar']);
+
+	Route::get('/admin/tipos/deletar/{id}', ['as' => 'admin.tipos.deletar', 'uses' => 'Admin\TipoController@deletar']);
+
+
 
 });
 
